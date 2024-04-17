24a3d0c135834493fdbd76ee2860279bdd096150
Leia <leiaooi.94@gmail.com>
2024-04-17 10:47:49 -0700

    Add folders for controllers and routes. Functional get request to /warehouses endpoint

---
+++ controllers/warehouse-controller.js
@@ -0,0 +1,15 @@
+const knex = require("knex")(require("../knexfile"));
+
+const index = async (_req, res) => {
+	try {
+		const data = await knex("warehouses");
+		res.status(200).json(data);
+	} catch (error) {
+		res.status(400).send(`Error retrieving Warehouses: ${error}`);
+	}
+};
+
+
+module.exports = {
+	index,
+};
