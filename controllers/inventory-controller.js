ee8a941f7f8575b3330699f2f7f79be67ac548e3
Celine Zhou <celine_zhou@outlook.com>
2024-04-18 11:12:58 -0700

    added warehouse name to inventory database using join

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -1,8 +1,21 @@
-const knex = require('knex')(require("../knexfile")); 
+const knex = require('knex')(require("../knexfile"));
 
 const getAllInventoryItems = async (_req, res) => {
   try {
     const inventoryItems = await knex('inventories')
+      .select(
+        'inventories.id', 
+        'inventories.item_name', 
+        'inventories.description', 
+        'inventories.category', 
+        'inventories.status', 
+        'inventories.quantity', 
+        'inventories.created_at', 
+        'inventories.updated_at',
+        'warehouses.warehouse_name'  
+      )
+      .join('warehouses', 'inventories.warehouse_id', 'warehouses.id'); 
+
     res.status(200).json(inventoryItems);
   } catch (error) {
     res.status(400).send(`Error getting inventory items: ${error}`);
