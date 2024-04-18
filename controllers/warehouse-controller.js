b05a862e10cd40b616b4b7e10a1af0da730b18ae
Erin Templeton <erindtempleton@gmail.com>
2024-04-18 10:05:24 -0700

    Update request to use params instead of body

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -11,15 +11,15 @@ const index = async (_req, res) => {
 
 const remove = async (req, res) => {
 	try {
-		const warehouse = await knex("warehouses").where({ id: req.body.warehouse }).delete();
+		const warehouse = await knex("warehouses").where({ id: req.params.id }).delete();
 		
 		if (!warehouse) {
-			return res.status(404).json({message: `Could not find warehouse: ${req.body.id}`})
+			return res.status(404).json({message: `Could not find warehouse: ${req.params.id}`})
 		}
 
-		res.status(204).json({message: `Successfully deleted warehouse: ${req.body.id}`})
+		res.status(204).json({message: `Successfully deleted warehouse: ${req.params.id}`})
 	} catch (error) {
-		res.status(400).send(`Error deleting warehouse: ${error}`);
+		res.status(404).send(`Error deleting warehouse: ${error}`);
 	}
 };
 
