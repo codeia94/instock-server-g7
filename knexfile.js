0b4506dea79e3f94484a0bb900fb6db92f3f06c9
Leia <leiaooi.94@gmail.com>
2024-09-13 01:13:41 -0700

    update knexfile to connect aws rds

--- knexfile.js
+++ knexfile.js
@@ -8,8 +8,9 @@ module.exports = {
   client: 'mysql2',
   connection: {
     host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+		database: process.env.DB_NAME,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		port: process.env.DB_PORT,
   }
 };
